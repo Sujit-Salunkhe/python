29aafa05ddf0ce367849283d04be976758e31256
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2025-12-21 18:58:57 +0530

    solving leet code problems

--- september2022/solveingleetcode/leetcode.py
+++ september2022/solveingleetcode/leetcode.py
@@ -107,3 +107,48 @@ def fib(n):
   return fib(n-1) + fib(n-2)
 
 print(fib(3))
+def factorial(n):
+    if n == 1:
+        return 1
+    return n*factorial(n-1)
+
+def fib(n):
+  if n == 0 or n==1:
+      return n
+  return fib(n-1) + fib(n-2)
+
+print(fib(3))
+def factorial(n):
+    if n == 1:
+        return 1
+    return n*factorial(n-1)
+
+def fib(n):
+  if n == 0 or n==1:
+      return n
+  return fib(n-1) + fib(n-2)
+
+print(fib(3))
+def factorial(n):
+    if n == 1:
+        return 1
+    return n*factorial(n-1)
+
+def fib(n):
+  if n == 0 or n==1:
+      return n
+  return fib(n-1) + fib(n-2)
+
+print(fib(3))
+def factorial(n):
+    if n == 1:
+        return 1
+    return n*factorial(n-1)
+
+def fib(n):
+  if n == 0 or n==1:
+      return n
+  return fib(n-1) + fib(n-2)
+
+print(fib(3))
+
